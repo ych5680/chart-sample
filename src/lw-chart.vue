049493d30cf652f68762001ae1b9ab9313ea8e86
ych5680 <568015364@qq.com>
2024-02-23 02:31:59 +0800

    first

---
+++ src/lw-chart.vue
@@ -0,0 +1,22 @@
+<script setup>
+    import { onMounted, ref } from 'vue';
+    import { createChart } from 'lightweight-charts';
+
+    let chart;
+    const chartContainer = ref();
+
+    onMounted(() => {
+        // Create the Lightweight Charts Instance using the container ref.
+        chart = createChart(chartContainer.value);
+    });
+
+    
+</script>
+<template>
+    <div class="lw-chart" ref="chartContainer"></div>
+</template>
+<style scoped>
+    .lw-chart {
+        height: 100%;
+    }
+</style>
